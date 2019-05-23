9801edb47129e82479d6cf77dbaa61c8852ca994
Diogo Cezar <diogo@diogocezar.com>
2019-05-23 19:52:10 -0300

    Node Rgb Projetct

--- node-rgb/back-end/app.js
+++ node-rgb/back-end/app.js
@@ -1,7 +1,8 @@
 const SerialPort = require("serialport");
 const portName = "/dev/cu.usbmodem146201";
 const Readline = require("@serialport/parser-readline");
-const delay = require("delay");
+const express = require("express");
+const app = express();
 
 port = new SerialPort(portName, {
   baudRate: 9600
@@ -12,14 +13,22 @@ const parser = port.pipe(new Readline({ delimiter: "\r\n" }));
 port.on("open", function() {
   console.log("open serial communication");
   parser.on("data", console.log);
-  (async () => {
-    for (;;) {
-      port.write(`240R17G17B`);
-      await delay(1000);
-      port.write(`17R252G94B`);
-      await delay(1000);
-      port.write(`244R17G252B`);
-      await delay(1000);
-    }
-  })();
+  // (async () => {
+  //   for (;;) {
+  //     port.write(`240R17G17B`);
+  //     await delay(1000);
+  //     port.write(`17R252G94B`);
+  //     await delay(1000);
+  //     port.write(`244R17G252B`);
+  //     await delay(1000);
+  //   }
+  // })();
 });
+
+app.get("/change/:r/:g/:b", function(req, res) {
+  const { r, g, b } = req.params;
+  port.write(`${r}R${g}G${b}B`);
+  res.send("ok");
+});
+
+app.listen(9999);
