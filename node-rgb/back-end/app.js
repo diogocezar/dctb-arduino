4964af5dba5328bb2840dee1e3d909b3926651a8
Diogo Cezar <diogo@diogocezar.com>
2019-05-23 23:52:53 -0300

    Front-End

--- node-rgb/back-end/app.js
+++ node-rgb/back-end/app.js
@@ -3,32 +3,28 @@ const portName = "/dev/cu.usbmodem146201";
 const Readline = require("@serialport/parser-readline");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-port = new SerialPort(portName, {
-  baudRate: 9600
-});
+try {
+  port = new SerialPort(portName, {
+    baudRate: 9600
+  });
 
-const parser = port.pipe(new Readline({ delimiter: "\r\n" }));
+  const parser = port.pipe(new Readline({ delimiter: "\r\n" }));
 
-port.on("open", function() {
-  console.log("open serial communication");
-  parser.on("data", console.log);
-  // (async () => {
-  //   for (;;) {
-  //     port.write(`240R17G17B`);
-  //     await delay(1000);
-  //     port.write(`17R252G94B`);
-  //     await delay(1000);
-  //     port.write(`244R17G252B`);
-  //     await delay(1000);
-  //   }
-  // })();
-});
+  port.on("open", function() {
+    parser.on("data", console.log);
+  });
+} catch (err) {
+  console.log("Error on serial communication.");
+}
+
+app.use(cors());
 
 app.get("/change/:r/:g/:b", function(req, res) {
   const { r, g, b } = req.params;
   port.write(`${r}R${g}G${b}B`);
-  res.send("ok");
+  res.status(200).json({ success: true });
 });
 
 app.listen(9999);
---
+++ node-rgb/front-end/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { SketchPicker } from "react-color";
+import axios from "axios";
+import "./styles.css";
+
+class App extends Component {
+  state = {
+    rgb: null,
+    color: "#223"
+  };
+  sendArduino = () => {
+    const { r, g, b } = this.state.rgb;
+    axios.get(`http://localhost:9999/change/${r}/${g}/${b}`).then(result => {
+      console.log(result);
+    });
+  };
+  onChangeComplete = props => {
+    this.setState({ rgb: props.rgb, color: props.hex });
+    this.sendArduino();
+  };
+  render() {
+    return (
+      <div className="container">
+        <h1>Selecione uma cor</h1>
+        <SketchPicker
+          className="picker"
+          color={this.state.color}
+          onChangeComplete={this.onChangeComplete}
+        />
+        <span>R: {this.state.rgb && this.state.rgb.r}</span>
+        <span>G: {this.state.rgb && this.state.rgb.g}</span>
+        <span>B: {this.state.rgb && this.state.rgb.b}</span>
+      </div>
+    );
+  }
+}
+
+export default App;
